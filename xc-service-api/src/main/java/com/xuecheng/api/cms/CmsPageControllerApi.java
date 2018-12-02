393249711516928cd6ed2ef35c80b518adb9bd9c
NewBlack51 <1154564163@qq.com>
2018-12-02 18:09:16 +0800

    OverView:添加swagger接口注解信息 BUG: Description:

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -2,6 +2,10 @@ package com.xuecheng.api.cms;
 
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
 import com.xuecheng.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author Black
@@ -9,6 +13,12 @@ import com.xuecheng.framework.model.response.QueryResponseResult;
  * @description com.xuecheng.api.cms
  * @date 2018/12/1
  */
+@Api(value = "cms页面管理接口", description = "cms页面管理接口，提供页面的增、删、改、查")
 public interface CmsPageControllerApi {
+    @ApiOperation("分页查询页面列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "页码", required = true, paramType = " path", dataType = "int"),
+            @ApiImplicitParam(name = "size", value = "每页记录数", required = true, paramType = " path", dataType = "int")
+    })
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest);
 }
